c480f102d9c22b07bf1f5631a2a6b7830e3676cc
mmxw11 <mmxw11@users.noreply.github.com>
2019-12-19 17:06:48 +0200

    Mark username as unique

--- src/main/java/sec/project/domain/BankUser.java
+++ src/main/java/sec/project/domain/BankUser.java
@@ -2,6 +2,7 @@ package sec.project.domain;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
@@ -13,6 +14,7 @@ import org.springframework.data.jpa.domain.AbstractPersistable;
 @Entity(name = "bank_user")
 public class BankUser extends AbstractPersistable<Long> {
 
+    @Column(unique = true)
     private String username;
     private String password;
     @OneToMany(mappedBy = "owner")
